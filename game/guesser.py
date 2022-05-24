9ab131da79f9c0dc70d717ec52ce80becdb5f3bc
cloggerish <104546948+cloggerish@users.noreply.github.com>
2022-05-23 23:28:58 -0600

    created basic methods and began coding

--- game/guesser.py
+++ game/guesser.py
@@ -1,3 +1,5 @@
+
+
 class Guesser:
     '''The person guessing the hidden word
     
@@ -14,3 +16,22 @@ class Guesser:
             compare current guess with guesses_list         
         add_guess to guesses_list
     '''
+
+    def __init__(self):
+        """Constructs a new Guesser
+
+        Args:
+            self (Guesser): An instance of Guesser.
+        """
+        self.guess_list = []
+        self.guess = ''
+
+    def get_guess(self):
+        """Gets a guess from the user"""
+
+    def if_duplicate_guess(self):
+        """Compare guess to guess list and determine if duplicate
+        If a duplicate, prompt for new guess"""
+
+    def add_guess(self):
+        "Add guess to guesses list"
