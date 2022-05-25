c3883637033decf85143215a3b0213272c48b187
Manders0689 <Mandy.beck05@gmail.com>
2022-05-25 16:13:08 -0600

    added game_outline text file

--- game/director.py
+++ game/director.py
@@ -4,7 +4,7 @@ from game.jumper import Jumper
 from game.word import Word
 
 class Director:
-    '''Person who directs the game.
+    """Person who directs the game.
     
     The responsibility of a Director is to control the sequence of play.
     
@@ -14,14 +14,14 @@ class Director:
         guesser (Guesser): The game's seeker.
         word (Word): word from a list of word the guesser will be guessing.
         terminal_service: For getting and displaying information on the terminal.
-    '''
+    """
     
     def __init__(self):
-        '''Constructs a new Director.
+        """Constructs a new Director.
         
         Ars:
             self (Director): an instance of Director.
-        '''
+        """
         self._jumper = Jumper()
         self._is_playing = True
         self._guesser = Guesser()
@@ -29,25 +29,33 @@ class Director:
         self._terminal_service = TerminalService()
         
     def start_game(self):
-        '''Starts the game by running the main game loop.
+        """Starts the game by running the main game loop.
         
         Ars:
             self (Director): an instance of Director.
-        '''
+        """
         while self._is_playing:
             self._get_inputs()
             self._do_updates()
             self._do_outputs()
             
     def _get_inputs(self):
-        ''''''
+        """ """
+        
+        
+    def _do_updates(self):
+        """ """
+        
+        
+    def _do_outputs(self):
+        """ """
         
         
         
         
         
     
-    '''Methods
+    """Methods
     
     start-game
         start the game by running the main game loop
@@ -60,4 +68,4 @@ class Director:
         update jumper if wrong guess
     _do_outputs
         show word and jumper update
-    '''
+    """
--- game/guesser.py
+++ game/guesser.py
@@ -1,21 +1,21 @@
 
 
 class Guesser:
-    '''The person guessing the hidden word
+    """The person guessing the hidden word
     
     The resposibility of a Guesser is to keep track of the letters guessed in hidden word.
     
     Attributes:
         guesses (list): stores guesses made.
         guess (string): induvidual letter guessed.
-        '''
+        """
     
-    '''Methods
+    """Methods
         get_guess
         if_duplicate_guess
             compare current guess with guesses_list         
         add_guess to guesses_list
-    '''
+    """
 
     def __init__(self):
         """Constructs a new Guesser
