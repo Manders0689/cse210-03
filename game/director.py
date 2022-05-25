9654ee591e419a2267d597757d71b2812ead7dd8
Manders0689 <Mandy.beck05@gmail.com>
2022-05-25 13:24:51 -0600

    update Director class

--- game/director.py
+++ game/director.py
@@ -1,3 +1,8 @@
+from game.terminal_service import TerminalService
+from game.guesser import Guesser
+from game.jumper import Jumper
+from game.word import Word
+
 class Director:
     '''Person who directs the game.
     
@@ -10,6 +15,37 @@ class Director:
         word (Word): word from a list of word the guesser will be guessing.
         terminal_service: For getting and displaying information on the terminal.
     '''
+    
+    def __init__(self):
+        '''Constructs a new Director.
+        
+        Ars:
+            self (Director): an instance of Director.
+        '''
+        self._jumper = Jumper()
+        self._is_playing = True
+        self._guesser = Guesser()
+        self._word = Word()
+        self._terminal_service = TerminalService()
+        
+    def start_game(self):
+        '''Starts the game by running the main game loop.
+        
+        Ars:
+            self (Director): an instance of Director.
+        '''
+        while self._is_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+            
+    def _get_inputs(self):
+        ''''''
+        
+        
+        
+        
+        
     
     '''Methods
     
