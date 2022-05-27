0b191e902ed70ce182605f98beac6ed059fce23a
dianawaite <wai19007@byui.edu>
2022-05-27 17:59:23 -0600

    jumper update

--- game/jumper.py
+++ game/jumper.py
@@ -17,16 +17,57 @@ class Jumper:
     def __init__(self):
 
       """  draw jumper """
+    def display_jumper(tries):
+        stages = [  # final state: no parachute
+                """
+        
+                0
+               /|\
+               / \
+            """,
 
-       ____
-      /____\
-      \    /
-       \  /
-        0
-       /|\
-       / \
+            """
+               \ /
+                0
+               /|\
+               / \
+            """,
 
+            """
+              \   /
+               \ /
+                0
+               /|\
+               / \
+            """,
+
+            """
+              /___\
+              \   /
+               \ /
+                0
+               /|\
+               / \
+            """,
        
+            """
+               ___
+              /___\
+              \   /
+               \ /
+                0
+               /|\
+               / \
+            """
+    ]
+    return stages[tries]
+
+
+       """ if letter is true, do nothing """
+
+
+       """if letter is false, remove top line
+       redraw jumper """
 
       
 
